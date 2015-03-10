b6a185fbdd58959e35f117750cd2571038cef9ca
William Vanderhoef <william@sumall.com>
2015-03-09 20:15:25 -0400

    Added template for messages and got everything working.

--- web.js
+++ web.js
@@ -1,6 +1,10 @@
+var Notifier = require('ac-koa-hipchat-notifier').Notifier;
+var Commands = require('./lib/commands');
+
 var ack = require('ac-koa').require('hipchat');
 var pkg = require('./package.json');
 var app = ack(pkg);
+
 
 var addon = app.addon()
   .hipchat()
@@ -15,7 +19,11 @@ var notifier = Notifier({format: 'html', dir: __dirname + '/messages'});
 var commands = Commands(notifier);
 
 addon.webhook('room_message', /^\/beerme$/, function *() {
-  yield commands.beerme();
+
+    yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!  Looking for recent untappd checkins within 1km...');
+
+    yield commands.beerme();
+
 });
 
 app.listen();
